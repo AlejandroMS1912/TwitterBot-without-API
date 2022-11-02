a79172e15f8eefabf1b92240e2bdb5bc68307a86
AlejandroMS1912 <110389988+AlejandroMS1912@users.noreply.github.com>
2022-11-02 15:28:36 +0100

    Add files via upload

---
+++ RT&LikeFrom@.py
@@ -0,0 +1,101 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import random
+
+
+class TwitterBot:
+    def __init__(self, username, password):
+        '''
+        This method asign the username and the password parameters inputs in the driver that will be used in the following methods
+        '''
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Chrome(
+            executable_path='C:/Users/Usuario/Desktop/Twitterbot/chromedriver.exe')
+
+    def login(self):
+        '''
+        In this method firstly the driver searchs the login page of twitter, then detects the user and the password boxes and clean
+        and fill them with the established parameters
+        '''
+        bot = self.bot
+        bot.get('https://twitter.com/login')
+        time.sleep(3)
+
+        email = bot.find_element("xpath", '//input[@autocomplete="username"]')
+        email.clear()
+        email.send_keys(self.username)
+        email.send_keys(Keys.RETURN)
+        time.sleep(3)
+
+        password = bot.find_element("xpath", '//input[@name="password"]')
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(3)
+
+    def RTandLIKE_from_user(self, user):
+        '''
+        This method firstly reject the cookies pop-up to avoid future click problems, then search the user inputed page. After
+        a quick scroll in that page detects all the tweets and store them in a list that will later be the index from wich being 
+        liked and retweeted.
+
+        This method also has a random interval of seconds to make it in the most natural way posible in order to avoid possible bans.
+        This is fully explained in the github documentation.
+        '''
+
+        bot = self.bot
+
+        RejectCookies = bot.find_element("xpath", '//*[@id="layers"]/div/div/div/div/div/div[2]/div[2]/div/span/span')        
+        RejectCookies.click()
+
+        bot.get("https://twitter.com/" + user)
+        time.sleep(3)
+
+        for i in range(1, 3):
+            bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+            time.sleep(2)
+
+        tweets = bot.find_elements("xpath", '//a[@dir="auto" and not(@rel)]')
+        tweets_url = [tweet.get_attribute('href') for tweet in tweets]
+        print("Tweets to RT and Like: (", (len(tweets_url) - 3), ")",  tweets_url)
+
+        for url in tweets_url:
+        
+            bot.get(url)
+            try:
+                time.sleep(1)
+                if url == 'https://twitter.com/i/keyboard_shortcuts':
+                    continue
+                    
+                if url== "https://twitter.com/" + user + "/following":
+                    continue
+                
+                if url== "https://twitter.com/" + user + "/followers":
+                    continue
+                 
+                else:
+                    time.sleep(2)
+                    likeButton = bot.find_element("xpath", '//div[@data-testid="like"]')   
+                    webdriver.ActionChains(bot).move_to_element(likeButton).click(likeButton).perform()
+                    time.sleep(1)
+
+                    rtButton = bot.find_element("xpath", '//div[@data-testid="retweet"]')   
+                    webdriver.ActionChains(bot).move_to_element(rtButton).click(rtButton).perform()
+                    time.sleep(2)
+                    rtButton = bot.find_element("xpath", '//div[@data-testid="retweetConfirm"]')   
+                    webdriver.ActionChains(bot).move_to_element(rtButton).click(rtButton).perform()
+                    time.sleep(random.randint(15,20))
+
+            except Exception as ex:
+                print(ex)
+                print(url + " is already liked and retweeted")
+                time.sleep(random.randint(2,5))
+
+            time.sleep(2)
+
+
+UserParameters = TwitterBot('Probando229', 'pepito3pepito3')
+UserParameters.login()
+UserParameters.RTandLIKE_from_user('Banky__moon')
---
+++ Unfollow.py
@@ -0,0 +1,96 @@
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import random
+
+
+class TwitterBot:
+    def __init__(self, username, password):
+        '''
+        This method asign the username and the password parameters inputs in the driver that will be used in the following methods
+        '''
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Chrome(
+            executable_path='C:/Users/Usuario/Desktop/Twitterbot/chromedriver.exe')
+
+    def login(self):
+        '''
+        In this method firstly the driver searchs the login page of twitter, then detects the user and the password boxes and clean
+        and fill them with the established parameters
+        '''
+        bot = self.bot
+        bot.get('https://twitter.com/login')
+        time.sleep(3)
+
+        email = bot.find_element("xpath", '//input[@autocomplete="username"]')
+        email.clear()
+        email.send_keys(self.username)
+        email.send_keys(Keys.RETURN)
+        time.sleep(3)
+
+        password = bot.find_element("xpath", '//input[@name="password"]')
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(3)
+
+    def unfollow(self, username):
+        '''
+        This method firstly reject the cookies pop-up to avoid future click problems, then search your profile page and your 
+        "following page". After a quick scroll in that page detects all the usernames and store them in a list that will
+        later be the index from wich unfollow each of them.
+
+        The method also has a counter to control the number of unfollows and a random interval of seconds to make it in the most 
+        natural way posible in order to avoid possible bans. This is fully explained in the github documentation.
+        '''
+
+        bot = self.bot
+
+        RejectCookies = bot.find_element("xpath", '//*[@id="layers"]/div/div/div/div/div/div[2]/div[2]/div/span/span')        
+        RejectCookies.click()
+        
+        bot.get("https://twitter.com/"+ username)
+        time.sleep(3)
+        bot.get("https://twitter.com/"+ username + "/following")
+        time.sleep(3)
+
+        arrobas = bot.find_elements("xpath", '//a[@role="link" and @aria-hidden="true"]')
+        lista_arrobas = [arroba.get_attribute('href') for arroba in arrobas]
+
+        lista_arr_def = []
+        for arroba in lista_arrobas:
+            arroba = "@" + arroba.split("/")[3]
+            lista_arr_def.append(arroba)
+        
+        print("Lista de @ detectados (", len(lista_arr_def), "): ", lista_arr_def)
+
+        cont_unfollows = 0
+
+        for n in lista_arr_def:
+                   
+            time.sleep(2)
+            unfollows_b = bot.find_element(By.XPATH, "//div[@aria-label='Following " + n + "']")
+            webdriver.ActionChains(bot).move_to_element(unfollows_b).click(unfollows_b).perform()
+            time.sleep(1)
+            unfollows_b_confirm = bot.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div[2]/div[1]/div')
+            unfollows_b_confirm.click()
+            print("You have unfollowed ", n)
+            time.sleep(2)
+
+            if cont_unfollows <= 19:
+                cont_unfollows += 1
+                print("You have " + str(cont_unfollows) + " consecutive unfollows, " 
+                        + str(20 - cont_unfollows) + " left for the next break (20 min)")
+                time.sleep(random.randint(6,30))
+
+
+            if cont_unfollows > 19:
+                print("You have 20 consecutive unfollows, its time to stop (20 min)")
+                cont_unfollows = 0
+                time.sleep(1200)
+                
+UserParameters = TwitterBot('Probando229', 'pepito3pepito3')
+UserParameters.login()
+UserParameters.unfollow('Probando229')
