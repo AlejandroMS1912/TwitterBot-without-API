f42f788a16f5dcc54eea5a4c863a0ce60036c091
AlejandroMS1912 <110389988+AlejandroMS1912@users.noreply.github.com>
2022-11-02 15:43:31 +0100

    Update Unfollow.py

--- Unfollow.py
+++ Unfollow.py
@@ -91,6 +91,6 @@ class TwitterBot:
                 cont_unfollows = 0
                 time.sleep(1200)
                 
-UserParameters = TwitterBot('Probando229', 'pepito3pepito3')
+UserParameters = TwitterBot('Probando2847', 'Hello01134') # ('username', 'password')
 UserParameters.login()
-UserParameters.unfollow('Probando229')
+UserParameters.unfollow('Probando2847') # ('username')
